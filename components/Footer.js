43b03b369ec6c4378e264e092941cea40ff4472c
Luke Mico <uvvhibt6qc@privaterelay.appleid.com>
2022-07-06 23:04:19 +1000

    Add email to Footer

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,10 @@ const Footer = () => {
 			<div className="max-w-4xl px-4 mx-auto text-gray-800 dark:text-white">
 				<div className="pb-8 mb-2 border-t-2 border-gray-300 dark:border-white-300"></div>
 				<div className="flex flex-col justify-between lg:flex-row items-center">
-					<p>Built with Next.js, Tailwind and Vercel</p>
+					<p>
+						Built with Next.js, Tailwind and Vercel. © luke mico
+						2022.
+					</p>
 					<div className="flex flex-wrap pt-2 sm:space-x-4 space-x-2 font-medium lg:pt-0">
 						<a
 							href="https://twitter.com/lukemico"
@@ -39,7 +42,7 @@ const Footer = () => {
 							GitHub
 						</a>
 						<a
-							href="#"
+							href="mailto:lukemico@me.com"
 							className={
 								'transition-colors hover:text-yellow-500'
 							}
